1c05fb5d6c16ffb89de6f82b38469015f8cecc83
andyalvarez11 <120332228+andyalvarez11@users.noreply.github.com>
2023-05-10 16:26:43 -0400

    completed assignment

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,52 @@
-// Step1: when generate password button is first clicked prompt user to select which criteria to include in password:
-  // 1st prompt: Input how many characters user would like password to contain: < 8 > 128 characters
-  // 2nd prompt: click OK to confirm including special characters
-  // 3rd prompt: click OK to confirm inclduing numeric characters
-  // 4th prompt: click OK to confrim including lowercase characters
-  // 5th prompt: click OK to confirm including uppercase characters
-// Step2: program generates random password dispalyed in box with given criteria
 
-
-// Assignment code here
-
+// passwordCharacters is a global var and usable anywhere in the code
+var passwordCharacters=" ";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// ask user for input about password parameters
+    var numberOfCharacters=prompt("How many characters would you like?")
+    if(numberOfCharacters >=8 && numberOfCharacters<=128){
+        console.log("good length");
+    } else {
+        alert("Password needs to be between 8 and 128 characters")
+        writePassword();
+    }
+    // ask user if they want special characters
+    var includeSpecialCharacters=confirm("Do you want special characters?")
+    if(includeSpecialCharacters){
+        passwordCharacters+="!@#$%^&*()_?><";
+    }
+    // ask user if they want upper case characters
+    var upperCase=confirm("Do you want to include Uppercase characters?")
+    if(upperCase){
+        passwordCharacters+="QWERTYUIOPASDFGHJKLZXCVBNM";
+    }
+    // ask user if they want lower case characters
+    var lowerCase=confirm("Do you want to include Lowercase characters?")
+    if(lowerCase){
+        passwordCharacters+="qwertyuiopasdfghjklzxcvbnm"
+    }
+    // generate password with user's requests
+
+    var finalPassword="";
+    for(var i=0;i<numberOfCharacters;i++){
+        // create a random character x number of times
+        var randomChar=passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)]
+        15, 45, 23, 8
+        passwordCharacters[15]
+        example = '!@#$%^&*()_+ABCDEFGHYDKLJAGPOEakldsdklndskojto'
+        finalPassword+=randomChar;
+    }
+    
+    // last step is to display the finalPassword on the page for the user to see
+    var passwordText = document.querySelector("#password");
+    passwordText.value = finalPassword;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function clickHandler(event) {
-  console.log('Button Clicked');
-}
-
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', clickHandler);
